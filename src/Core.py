14bf3c2953a60d3b66be6aeb073e7c25ade742f3
omeradjallala <omer.adjallala@epitech.eu>
2024-01-06 14:29:52 +0100

    g

--- src/Core.py
+++ src/Core.py
@@ -17,15 +17,18 @@ class Core:
 
     def run(self):
         right, middle, left = self.sensor.feed()
-        if left:
-            self.driver.move(70, 'no', 'left', 0.8)
-            self.rot = 'left'
-        if right:
-            self.driver.move(70, 'no', 'right', 0.8)
-            self.rot = 'right'
-        if middle:
-            self.driver.move(20, 'forward', 'no', 0.8)
-        if right == 0 and middle == 0 and left == 0:
-            self.driver.move(20, 'no', self.rot, 0.8)
+
+        self.driver.move(70, 'no', 'left', 0.8)
+
+        # if left:
+        #     self.driver.move(70, 'no', 'left', 0.8)
+        #     self.rot = 'left'
+        # if right:
+        #     self.driver.move(70, 'no', 'right', 0.8)
+        #     self.rot = 'right'
+        # if middle:
+        #     self.driver.move(20, 'forward', 'no', 0.8)
+        # if right == 0 and middle == 0 and left == 0:
+        #     self.driver.move(20, 'no', self.rot, 0.8)
             # self.driver.move(50, 'backward', 'no', 0.8)
         print(right, middle, left, self.rot)
